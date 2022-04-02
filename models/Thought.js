13fb8b9f937353874195361a39d477dc2401d45c
Ayako Woollan <ayako.woollan@gmail.com>
2022-04-02 22:31:53 +1100

    updating thoughtSchema need dateformat and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: [true, 'Please enter Your Thoughts.'],
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //need to format date
+            // get:
+        },
+        username: {
+            type: String,
+            required: [true, 'Please enter username.'],
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
