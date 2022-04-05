af8a4d12be04f4273391980a568889be729cf513
Ayako Woollan <ayako.woollan@gmail.com>
2022-04-05 22:43:07 +1000

    update routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+router.route('/:userId/reactions').post(addReaction);
+
+router.route('/:userId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
